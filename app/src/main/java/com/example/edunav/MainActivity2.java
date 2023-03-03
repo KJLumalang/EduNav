1b656986f300ed87c6db76b1331de7238078c13d
EduNav <timekillerph123@gmail.com>
2023-03-03 11:17:08 +0800

    main page

--- app/src/main/java/com/example/edunav/MainActivity2.java
+++ app/src/main/java/com/example/edunav/MainActivity2.java
@@ -2,11 +2,18 @@ package com.example.edunav;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    ImageButton myImageButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,19 @@ public class MainActivity2 extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main2);
+
+
+        myImageButton = (ImageButton)findViewById(R.id.imageButton2);
+        myImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent school_info = new Intent(MainActivity2.this, school_info.class);
+                startActivity(school_info);
+            }
+            });
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/edunav/school_info.java
@@ -0,0 +1,14 @@
+package com.example.edunav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class school_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school_info);
+    }
+}
